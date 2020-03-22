4dd8abcb48921e8efe7216b318c45f81aeba59e5
shaozhun <712104878@qq.com>
2020-03-23 00:24:45 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
-
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
