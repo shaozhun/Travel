69b997bb2e16a9975475e8d1bc5341e8ffd64fe3
shaozhun <712104878@qq.com>
2020-03-25 23:41:29 +0800

    add sroll

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,11 +2,12 @@
   <div>
     <City-Header></City-Header>
     <City-Search></City-Search>
-    <City-List></City-List>
-    <City-Alphabet></City-Alphabet>
+    <City-List :hot="hotcities" :cities="cities" :letter="letter"></City-List>
+    <City-Alphabet :cities="cities" @change="handleLetterChange"></City-Alphabet>
   </div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
@@ -14,8 +15,31 @@ import CityAlphabet from './components/Alphabet'
 
 export default {
   name: 'City',
+  data () {
+    return {
+      hotcities: null,
+      cities: {},
+      letter: ''
+    }
+  },
   components: {
     CityHeader, CitySearch, CityList, CityAlphabet
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('http://101.37.204.199/api/cityData.php')
+        .then(this.handleGetCItyInfoSucc)
+    },
+    handleGetCItyInfoSucc (res) {
+      this.hotcities = res.data.data.hotcities
+      this.cities = res.data.data.cities
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,35 +1,53 @@
 <template>
   <div class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
-
-    <li class="item">A</li>
+    <li class="item" v-for="item of letters" :key="item" :ref="item"
+    @touchstart="handleTouchStart"
+    @touchmove="handleTouchMove"
+    @touchend="handleTouchEnd"
+    @click="handleLetterClick">{{item}}</li>
   </div>
 </template>
 <script>
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
