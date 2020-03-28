027710fd96da3044dd410cee4d8b8c2a0d287934
shaozhun <712104878@qq.com>
2020-03-29 00:34:02 +0800

    add hotnum

--- src/pages/home/components/HotList.vue
+++ src/pages/home/components/HotList.vue
@@ -2,7 +2,11 @@
   <div class="hotlist">
      <div class="title"><img class="title-heart" src="http://img1.qunarzz.com/piao/fusion/1711/16/bfbb9874e8f11402.png" alt="本周热门榜单">本周热门榜单</div>
     <div class="scroll_box">
-        <router-link tag="span" :to="`/detail/`+item.id" class="box" v-for="item in hotList" :key="item.id"><img class="box-img" :src="item.url" >
+        <router-link tag="span" :to="`/detail/`+item.id" class="box" v-for="item in hotList" :key="item.id">
+        <img class="mp-hotsale-tagimg" src="http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png" :alt="item.title" v-show="item.id == 1">
+        <img class="mp-hotsale-tagimg" src="http://img1.qunarzz.com/piao/fusion/1710/2d/36d0c4adaebbbc02.png" :alt="item.title" v-show="item.id == 2">
+        <img class="mp-hotsale-tagimg" src="http://img1.qunarzz.com/piao/fusion/1710/67/edc47ffef9e96b02.png" :alt="item.title" v-show="item.id == 3">
+          <img class="box-img" :src="item.url" >
           <span class="box-title">{{item.title}}</span>
           <span class="box-price">¥{{item.price}}<span class="price-inner">起</span></span>
         </router-link>
@@ -57,6 +61,17 @@ export default {
     overflow-y: hidden
     .box
       position relative
+    .mp-hotsale-tagimg
+      position: absolute
+      top: -.81rem
+      left: 0
+      width: .8rem
+      height: 0.35rem
+      background-size: 100%
+      color: #fff
+      font-size: 0.2rem
+      line-height: 0.38rem
+      text-indent: 0.04rem
     .box-img
       width 100px
       height 100px
